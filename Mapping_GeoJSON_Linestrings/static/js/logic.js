45791aca192417a3cf11123c9b702b0c04a1d86e
Jared Clarke <jared@Jareds-Air.fios-router.home>
2021-01-10 18:09:28 -0500

    myStyle

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -23,7 +23,7 @@ let baseMaps = {
 let map = L.map('mapid', {
     center: [44.0, -80.0],
     zoom: 2,
-    layers: [light]
+    layers: [dark]
 });
 
 // Pass our map layers into our layers control and add the layers control to the map.
@@ -32,13 +32,24 @@ L.control.layers(baseMaps).addTo(map);
 // Accessing the airport GeoJSON URL
 // Accessing the Toronto airline routes GeoJSON URL.
 let torontoData = "https://raw.githubusercontent.com/jaredcclarke/Mapping_Earthquakes/Mapping_GeoJSON_Linestrings/torontoRoutes.json";
+// Create a style for the lines.
+
+let myStyle = {
+    color: "#ffffa1",
+    weight: 2
+}
 
 // Grabbing our GeoJSON data.
 d3.json(torontoData).then(function(data) {
     console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
-  L.geoJson(data)
-    .addTo(map);
+  L.geoJson(data, {
+      style: myStyle,
+      onEachFeature: function(feature, layer) {
+          layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3> <hr><h3> Destination: " + feature.properties.dst + "</h3>");
+      }
+  }).addTo(map);
 });
+
 
 
